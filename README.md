e8ef0daf3db11a1b89a809f2767dc4fed732a865
dunika <dbreen91@gmail.com>
2017-08-15 20:15:35 +0100

    Updated README and validation function

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Paginated Listings Scraper
 
 Extract listings data from paginated web pages
+
+
+
+It uses [Cheerio](https://github.com/cheeriojs/cheerio) in order to access the DOM which implements a subset of JQuery and selects elements in exactly the same way.
+
+Options
+
+Filter (optional)
+
+Can be either a CSS selector or a function. It is used to filter out unwanted elements before the inital iteration takes place. See [filter](https://github.com/cheeriojs/cheerio#filter-selector---filter-selection---filter-element---filter-functionindex-element-) for explanation and example usage 
--- src/validate-options.js
+++ src/validate-options.js
@@ -11,26 +11,39 @@ export default function validateOptions(options) {
   if (isPlainObject(options)) {
     throw new Error('Expected options to be an object');
   }
-  const { filter, maximiumDepth, parentSelector, selectData, terminate, url } = options;
-  if (!isString(url)) {
-    throw new Error('Expected url to be a string');
+
+  const {
+    filter,  // Optional. Can be either a string or a function
+    nextPageSelector, // Optional. Can be either a string or a function
+    maximiumDepth, // Required if terminate is undefined. Must be a positive integer
+    parentSelector, // Required. Must be a string
+    dataSelector, // Required. Can be either a plain object or a function
+    terminate, // Required if maximiumDepth is undefined. Must be a positive integer
+    url, // Required. Must be a string
+  } = options;
+
+  if (!isUndefined(filter) && (!isString(filter) || !isFunction(filter))) {
+    throw new Error('Expected filter to be a function');
   }
-  if (!isString(parentSelector)) {
-    throw new Error('Expected parentSelector to be a string');
-  }
-  if (!isFunction(selectData)) {
-    throw new Error('Expected selectData to be a function');
-  }
-  if (isUndefined(maximiumDepth) && isUndefined(terminate)) {
-    throw new Error('Expected maximiumDepth or terminate function to be provided');
+  if (!isUndefined(filter) && (!isString(nextPageSelector) && !isFunction(nextPageSelector))) {
+    throw new Error('Expected filter to be a function');
   }
   if (!isUndefined(maximiumDepth) && !isSafePositiveInteger(maximiumDepth)) {
     throw new Error('Expected maximiumDepth to be a positive integer');
   }
+  if (isUndefined(maximiumDepth) && isUndefined(terminate)) {
+    throw new Error('Expected maximiumDepth or terminate function to be provided');
+  }
+  if (!isString(parentSelector)) {
+    throw new Error('Expected parentSelector to be a string');
+  }
+  if (!isPlainObject(dataSelector) || !isFunction(dataSelector)) {
+    throw new Error('Expected selectData to be a function');
+  }
   if (!isUndefined(terminate) && isFunction(terminate)) {
     throw new Error('Expected terminate to be a function');
   }
-  if (!isUndefined(filter) && isFunction(filter)) {
-    throw new Error('Expected filter to be a function');
+  if (!isString(url)) {
+    throw new Error('Expected url to be a string');
   }
 }
